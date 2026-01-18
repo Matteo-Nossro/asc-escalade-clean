0406ad8582cd52ceda7a162c3d3d59b644b464e0
Matteo-Nossro <matnoss@gmail.com>
2026-01-18 16:20:12 +0100

    ajout page de liens, pas de visuel ni rien de fait

---
+++ app/pages/liens.vue
@@ -0,0 +1,70 @@
+<template>
+	<div class="text-center max-w-2xl mx-auto mb-12 pt-12">
+		<h1 class="text-4xl md:text-5xl font-black text-[#0F1729] mb-4 uppercase tracking-tight">
+			Liens utiles
+		</h1>
+		<p class="text-gray-500 text-lg">
+			FFME, ct FFME 21, falaises
+		</p>
+		<!-- Petite ligne verte déco -->
+		<div class="w-16 h-1 bg-[#7FD857] mx-auto mt-6 rounded-full"></div>
+	</div>
+
+
+	<UContainer>
+		<div class="">
+
+			<!-- Colonne Gauche : Texte -->
+			<div class="flex flex-col gap-6">
+				<!-- Titre avec icône -->
+				<div class="flex items-center gap-3 mb-2">
+					<h2 class="text-xl font-bold text-[#0F1729]">
+						Sites FFME
+					</h2>
+				</div>
+
+				<div class="space-y-2 text-gray-600 leading-relaxed text-justify">
+					<p>
+						Site internet de la fédération <a href="https://www.ffme.fr/">Site de la FFME</a>
+					</p>
+					<p>
+						Site internet du comité territorial de la Côte d'Or <a href="https://ctffme21.wordpress.com/">Site CT FFME 21</a>
+					</p>
+					<p>
+						Affiliés à la FFME et labellisés Handisport, nous nous engageons pour une pratique sécurisée et inclusive.
+					</p>
+				</div>
+
+
+				<div class="flex items-center gap-3 mb-2">
+					<h2 class="text-xl font-bold text-[#0F1729]">
+						Notre Histoire
+					</h2>
+				</div>
+
+				<div class="space-y-2 text-gray-600 leading-relaxed text-justify">
+					<p>
+						Fondé en 1998 par une poignée de passionnés, Vertical Pulse n'était au départ qu'un petit groupe d'amis se réunissant pour grimper sur les falaises locales.
+					</p>
+					<p>
+						Aujourd'hui, l'association a bien grandi mais l'esprit reste le même : convivialité, partage et dépassement de soi. Nous avons pour mission de rendre l'escalade accessible à tous, quel que soit l'âge, le niveau ou la condition physique.
+					</p>
+					<p>
+						Affiliés à la FFME et labellisés Handisport, nous nous engageons pour une pratique sécurisée et inclusive.
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</UContainer>
+
+</template>
+
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
