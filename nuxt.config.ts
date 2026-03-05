f1524dec4ec0fd773a73ccb2e2c721214165772f
Matteo-Nossro <matnoss@gmail.com>
2026-03-05 11:08:25 +0100

    ajout dépendence et initialisation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import mkcert from "vite-plugin-mkcert";
+
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui','@storyblok/nuxt'],
 
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  storyblok: {
+      bridge: true,
+      accessToken: process.env.STORYBLOK_TOKEN,
+      apiOptions: {
+        region: process.env.STORYBLOK_REGION || "eu",
+        endpoint: process.env.STORYBLOK_API_BASE_URL
+          ? `${new URL(process.env.STORYBLOK_API_BASE_URL).origin}/v2`
+          : undefined,
+      },
+    },
 
   colorMode: {
     preference: 'light', // Mode par défaut
@@ -16,5 +30,13 @@ export default defineNuxtConfig({
 
   routeRules: {
     '/': { prerender: true }
+  },
+
+    devServer: {
+    https: true,
+  },
+
+  vite: {
+    plugins: [mkcert()],
   },
 })
