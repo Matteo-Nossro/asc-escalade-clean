cb44f105e5e307445a10d9e125efd22e5bb5d35b
Matteo-Nossro <matnoss@gmail.com>
2026-03-25 15:24:35 +0100

    feat(nuxt): install @nuxtjs/supabase module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,21 @@
 import mkcert from "vite-plugin-mkcert";
 
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui','@storyblok/nuxt','@nuxt/image'],
+  modules: ['@nuxt/ui', '@storyblok/nuxt', '@nuxt/image', '@nuxtjs/supabase'],
 
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false,
+    cookieOptions: {
+      maxAge: 60 * 60 * 8,
+      sameSite: 'lax',
+      secure: true,
+    },
+  },
 
   storyblok: {
       bridge: true,
